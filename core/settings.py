9728d1ac51876899c514ed4351ad177047c9b021
ibrahim albayati <computer@MacBook-Pro-ibrahim.local>
2025-11-28 19:28:14 +0300

    a

--- core/settings.py
+++ core/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f5t8u06umgv36ci7bza1c%2ha&gx*o+j4jkkjx$-c=tlzvp7%$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cs.saibra-iq.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
